4502370ef49b126f3556b5b06e07ae5636b423b0
Felix Rohrbach <fxrh@gmx.de>
2014-11-20 01:29:45 +0100

    Better urls + current points

--- challenges/urls.py
+++ challenges/urls.py
@@ -4,8 +4,8 @@ from challenges import views
 
 urlpatterns = patterns('',
     url(r'^$', views.index, name='index'),
-    url(r'^(?P<challenge_id>\d+)/$', views.info, name="info"),
-    url(r'^(?P<challenge_id>\d+)/edit/$', views.editChallenge, name="edit"),
+    url(r'^challenge/(?P<challenge_id>\d+)/$', views.info, name="info"),
+    url(r'^challenge/(?P<challenge_id>\d+)/edit/$', views.editChallenge, name="edit"),
     url(r'^create/$', views.createChallenge, name="create"),
     url(r'^login/$', 'django.contrib.auth.views.login', {'template_name': 'challenges/login.html',
                                                          'current_app': 'challenges'}, name="login"),
--- challenges/views.py
+++ challenges/views.py
@@ -12,7 +12,8 @@ from challenges.forms import InfoForm, CreateChallengeForm, EditChallengeForm
 
 def standardContext(request):
     if request.user.is_authenticated():
-        return {'username': request.user.get_username()}
+        return {'username': request.user.get_username(),
+                'user_points': User.from_authuser(request.user).current_points }
     return {}
 
 
